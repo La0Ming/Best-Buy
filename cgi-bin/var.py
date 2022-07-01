ebded433915eed7c469d4c60efd61f58d23a80b3
Johnny Nguyen <johnnyxut@gmail.com>
2022-07-01 14:12:14 +0200

    Removed prints and db password

--- cgi-bin/var.py
+++ cgi-bin/var.py
@@ -1,3 +1,3 @@
 MYSQL_USER =  'root' #replace with your user name.
-MYSQL_PASS =  'Iphone6s' #replace with your MySQL server password
+MYSQL_PASS =  '' #replace with your MySQL server password
 MYSQL_DATABASE = 'DV1587'#replace with your database name
