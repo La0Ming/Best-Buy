382a4daf73e395e859fd72e783b631525dc1e10a
Johnny Nguyen <johnnyxut@gmail.com>
2022-08-17 18:48:25 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,2 @@
+1. Make sure to add the path to index.py or index.cgi in web.py
+2. Make sure to add your MYSQL server details in mysql_details.py
--- cgi-bin/web.py
+++ cgi-bin/web.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def run_script():
-    file = open(r'C:\Users\johnn\Downloads\database-project-master-windows\database-project-master-windows\cgi-bin\index.py', 'r').read()
+    file = open(r'path/to/cgiscript', 'r').read()
     return exec(file)
 
 if __name__ == "__main__":
